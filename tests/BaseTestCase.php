5f7bd60fcd4566ca15ae28558320fbfaa0704acb
Moiseev I <i.i.moiseev@efko.ru>
2019-10-29 17:49:18 +0300

    Added Manager tests

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -4,6 +4,8 @@ namespace Imoisey\Cross\Tests;
 
 use Imoisey\Cross\Collection\Collection;
 use Imoisey\Cross\ItemInterface;
+use Imoisey\Cross\Manager;
+use Imoisey\Cross\Provider\ProviderInterface;
 use PHPUnit\Framework\TestCase;
 
 class BaseTestCase extends TestCase
@@ -11,7 +13,7 @@ class BaseTestCase extends TestCase
     const CLASS_ITEM = '\Imoisey\Cross\ItemInterface';
     const CLASS_COLLECTION = '\Imoisey\Cross\Collection\Collection';
 
-    protected function getItem($begin, $end)
+    public function getItem($begin, $end)
     {
         $stub = $this->getMockForAbstractClass(self::CLASS_ITEM);
 
@@ -33,4 +35,64 @@ class BaseTestCase extends TestCase
 
 class BaseCollection extends Collection
 {
+}
+
+class BaseWithCrossProvider implements ProviderInterface
+{
+    private $helper;
+
+    public function __construct(BaseTestCase $helper)
+    {
+        $this->helper = $helper;
+    }
+
+    public function getName()
+    {
+        return 'basewithcross';
+    }
+
+    public function getCollections()
+    {
+        $items = [
+            $this->helper->getItem("10:00", "11:30"),
+            $this->helper->getItem("11:10", "13:00"),
+            $this->helper->getItem("12:00", "15:00"),
+        ];
+
+        return [new BaseCollection($items)];
+    }
+}
+
+class BaseNoCrossProvider implements ProviderInterface
+{
+    private $helper;
+
+    public function __construct(BaseTestCase $helper)
+    {
+        $this->helper = $helper;
+    }
+
+    public function getName()
+    {
+        return 'basenocross';
+    }
+
+    public function getCollections()
+    {
+        $items = [
+            $this->helper->getItem("10:00", "11:30"),
+            $this->helper->getItem("12:00", "13:00"),
+            $this->helper->getItem("14:00", "15:00"),
+        ];
+
+        return [new BaseCollection($items)];
+    }
+}
+
+class BaseManager extends Manager
+{
+    public function getProviders()
+    {
+        return $this->providers;
+    }
 }
---
+++ tests/ManagerTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Imoisey\Cross\Tests;
+
+class ManagerTest extends BaseTestCase
+{
+    protected $providerWithCross;
+    protected $providerNoCross;
+
+    public function setUp()
+    {
+        $this->providerWithCross = new BaseWithCrossProvider($this);
+        $this->providerNoCross = new BaseNoCrossProvider($this);
+    }
+
+    public function testContstuct()
+    {
+        $manager = new BaseManager($this->providerWithCross);
+        $this->assertContains($this->providerWithCross, $manager->getProviders());
+
+        return $manager;
+    }
+
+    /**
+     * @depends testContstuct
+     */
+    public function testAddToProvider($manager)
+    {
+        $manager->addProvider($this->providerNoCross);
+        $this->assertContains($this->providerNoCross, $manager->getProviders());
+    
+        return $manager;
+    }
+
+    /**
+     * @depends testAddToProvider
+     */
+    public function testVerifyWithCross($manager)
+    {
+        $this->assertTrue($manager->verify());
+        $this->assertNotEmpty($manager->getCollision());
+        $this->assertEquals($manager->getCollision(), $manager->getCollision($this->providerNoCross->getName()));
+        $this->assertNotEmpty($manager->getCollision($this->providerWithCross->getName()));
+    }
+
+
+    public function testVerifyNoCross()
+    {
+        $manager = new BaseManager($this->providerNoCross);
+
+        $this->assertFalse($manager->verify());
+        $this->assertEmpty($manager->getCollision());
+        $this->assertEmpty($manager->getCollision($this->providerNoCross->getName()));
+    }
+}
